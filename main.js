93bc215d7fefb0cbc5d702830d2cdfc1cbb8d892
aadarshwebdeveloper <67097898+aadarshwebdeveloper@users.noreply.github.com>
2020-11-01 09:41:08 +0530

    WeatherApp
    All city weather shows

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-
+  
+  
+  
+  
+  let weather_icon = document.getElementById('weather_icon');
+  
+  weather_icon.classList.add("hide");
  function hi(){
    
    
@@ -18,11 +24,18 @@ let a = fetch(`http://api.openweathermap.org/data/2.5/weather?q=${input}&units=m
   
   let description_div = document.getElementById('desc');
   
+  let weather_icon = document.getElementById('weather_icon');
+  
+  weather_icon.classList.remove("hide");
   
   let humidity = document.getElementById('humidity');
   
   
   let pressure = document.getElementById('pressure');
+  
+  let feel = document.getElementById('feel');
+  
+  
   
   let name = data1.name;
   let temp = data1.main.temp;
@@ -30,13 +43,32 @@ let a = fetch(`http://api.openweathermap.org/data/2.5/weather?q=${input}&units=m
   let windspeed = data1.wind.speed;
   let humid = data1.main.humidity;
   let press = data1.main.pressure;
+  let feels_like = data1.main.feels_like;
+  let weather_id = data1.weather[0].icon;
+    
+  let weather_id_url = `http://openweathermap.org/img/wn/${weather_id}@2x.png`;  
+  let err = data1.message;
   
+    console.log(err);
+    console.log(weather_id_url);
   
+  weather_icon.src = weather_id_url;
  temp_div.innerHTML = temp + "°C";
  city_div.innerHTML = name + "°";
  description_div.innerHTML = "Weather : " + desc;
  humidity.innerHTML = "Humidity : " + humid + "%";
- pressure.innerHTML = "Pressure : " +  press + " atm.";
+ pressure.innerHTML = "Pressure : " +  press + " hPa";
+ feel.innerHTML = "feels_like " + feels_like +  " °C";
+}).catch(function(){
+  console.log("error");
+   swal("Incorrect City Name!", "Check The Name Of City", "error")
+   
+  setTimeout(function() {
+    location.reload()
+  }, 3000); 
+   
+  
+      
 });
 
 
